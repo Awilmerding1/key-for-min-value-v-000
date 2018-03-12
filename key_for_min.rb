c617e7a4e08b46bec29f941d0decbfd3fe147a41
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-03-12 19:05:27 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,13 +9,7 @@ def key_for_min_value(name_hash)
   else
   name_hash.collect do |key, value|
   if empty_variable == nil 
-    empty_variable = value
-  
-    return empty_key  empty_key = key
-    empty_key
-  elsif  empty_variable < value
-    empty_variable = value
-    empty_key = key
+    
   end
 end
 end
