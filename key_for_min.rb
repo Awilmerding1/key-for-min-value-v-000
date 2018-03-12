cba525d918d610cf69876efacd3dcad9d83cd79c
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-03-12 18:25:04 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,14 +2,17 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  max_value = ""
+  min_value = ""
+  min_key = ""
   array = []
   if name_hash = {}
     nil 
   else
   name_hash.collect do |key, value|
-    max_value = ""
-    value 
+    if min_value < value
+    return min_key 
+  else
+    key
   end
 end
 end
